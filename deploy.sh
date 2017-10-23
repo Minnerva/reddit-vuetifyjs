ad2cd3541c4cfb8ec946fff291133ea440bd1feb
Min <potchara40@hotmail.com>
2017-10-23 17:29:04 +0700

    :bug: change from not working to working

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
+git checkout master
+
+git branch -D gh-pages
+git push origin --delete gh-pages
+
 npm run dist
-git add dist
+git add dist -f
 git commit -m "to gh-page"
 git subtree push --prefix dist origin gh-pages
+
+git checkout master
