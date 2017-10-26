bf47b64ca14f2ffb56116d7862b965ad2c586280
Min <potchara40@hotmail.com>
2017-10-26 17:41:48 +0700

    to gh-page

--- dist/sw.js
+++ dist/sw.js
@@ -4,20 +4,20 @@ const workboxSW = new WorkboxSW({ clientsClaim: true })
 
 workboxSW.precache([
   {
-    "url": "https://minnerwa.github.io/reddit-vuetifyjs/app.73185903b2e0f80d5c4b.js",
-    "revision": "f45cf14cf8d7580dad79b1170a59b53f"
+    "url": "https://minnerwa.github.io/reddit-vuetifyjs/app.6892ba7d963e10a7b83d.js",
+    "revision": "972c070d7448ae66ff78f0b833f4f0c8"
   },
   {
     "url": "https://minnerwa.github.io/reddit-vuetifyjs/index.html",
-    "revision": "d543a34f783835c5b93225668b5854ed"
+    "revision": "0daa3059540c6cfd56ede85c13ee02b9"
   },
   {
     "url": "https://minnerwa.github.io/reddit-vuetifyjs/style.css",
     "revision": "2ea5f4721d7bab5e1c17d9e6b3d0e2b9"
   },
   {
-    "url": "https://minnerwa.github.io/reddit-vuetifyjs/vendor.711626db86f7f4e18553.js",
-    "revision": "92a209c79bc92ad03d5ce1b7178091e6"
+    "url": "https://minnerwa.github.io/reddit-vuetifyjs/vendor.a8d44633f5d38dcb3f3d.js",
+    "revision": "53ec90615fecb20ccc933dba169c8647"
   },
   {
     "url": "https://minnerwa.github.io/reddit-vuetifyjs/workbox-sw.prod.js",
@@ -29,6 +29,15 @@ workboxSW.router.registerRoute(
   'https://minnerwa.github.io/reddit-vuetifyjs/(.*)',
   workboxSW.strategies.networkFirst({networkTimeoutSeconds: 3})
 )
+
+// workboxSW.router.registerNavigationRoute('index.html', {
+//   whitelist: [/./]
+// })
+
+// workboxSW.router.registerRoute(
+//   /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
+//   workboxSW.strategies.networkFirst()
+// )
 
 importScripts('https://unpkg.com/workbox-routing@0.0.2/build/importScripts/workbox-routing.dev.v0.0.2.js');
 
