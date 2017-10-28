b66fabc97e1481826458d3a724339d29757560c5
Min <potchara40@hotmail.com>
2017-10-28 17:42:12 +0700

    :bug: remove too much, so revert back

--- src/components/flex/UpVoteDownVote.vue
+++ src/components/flex/UpVoteDownVote.vue
@@ -1,9 +1,11 @@
 <template>
-  <v-flex class="text-xs-center">
-    <v-icon :color="upvoteColor">arrow_upward</v-icon>
-    <div>{{ score }}</div>
-    <v-icon :color="downvoteColor">arrow_downward</v-icon>
-  </v-flex>
+  <v-layout row align-center fill-height>
+    <v-flex class="text-xs-center">
+      <v-icon :color="upvoteColor">arrow_upward</v-icon>
+      <div>{{ score }}</div>
+      <v-icon :color="downvoteColor">arrow_downward</v-icon>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
